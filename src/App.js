ab747ae575c1eb73613bd23900948c8cf8b75e39
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-08 15:20:36 +0530

    refreshing,nav,split pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import AddBook from './components/AddBook';
 import SearchBook from './components/SearchBook';
 import DeleteBook from './components/DeleteBook';
 import ViewAll from './components/ViewAll';
+import Nav from './components/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <AddBook/>
-      <SearchBook/>
-      <DeleteBook/>
-      <ViewAll/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AddBook />} />
+          <Route path='/SearchBook' element={<SearchBook />} />
+          <Route path='/DeleteBook' element={<DeleteBook />} />
+          <Route path='/ViewAll' element={<ViewAll />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  
+
+
   );
 }
 
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Nav from './Nav'
 
 const ViewAll = () => {
     return (
         <div>
+            <Nav/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <h1>BOOK DETAILS</h1>
                         <table class="table">
                             <thead>
                                 <tr>
