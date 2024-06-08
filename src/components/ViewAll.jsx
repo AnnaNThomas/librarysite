a850f8b6f0aa1ff1fc363d487ce684482dd6edcb
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-08 13:57:13 +0530

    VieAll added

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Book Title</th>
+                                    <th scope="col">Author</th>
+                                    <th scope="col">Publish Year</th>
+                                    <th scope="col">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">4</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">5</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">6</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">7</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">8</th>
+                                    <td>My half GirlFriend</td>
+                                    <td>Ottomon</td>
+                                    <td>2019</td>
+                                    <td>600</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
